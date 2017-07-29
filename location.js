d69f7eb0289753fe36a51cac3525d1de9dcf7f29
Atharva Jagtap <atharva.rjagtap@gmail.com>
2017-07-30 00:10:53 +0530

    Geolocation api

---
+++ location.js
@@ -0,0 +1,24 @@
+var currentpos = document.getElementById("currentpos");
+var watchpos = document.getElementById("watchpos");
+function geo_success(position) {
+currentpos.innerHTML="Lat:"+ position.coords.latitude+"<br>"+"Long:"+ position.coords.longitude+"<br>"+"Accuracy:"+ position.coords.accuracy+"<br>"+"Heading:"+ position.coords.heading+"<br>"+"altitude:"+ position.coords.altitude+"<br>"+"Altitude Accuracy:"+ position.coords.altitudeAccuracy+"<br>"+"Speed:"+ position.coords.speed;
+}
+function wgeo_success(position) {
+watchpos.innerHTML="Lat:"+ position.coords.latitude+"<br>"+"Long:"+ position.coords.longitude+"<br>"+"Accuracy:"+ position.coords.accuracy+"<br>"+"Heading:"+ position.coords.heading+"<br>"+"altitude:"+ position.coords.altitude+"<br>"+"Altitude Accuracy:"+ position.coords.altitudeAccuracy+"<br>"+"Speed:"+ position.coords.speed;
+}
+
+function geo_error() {
+  alert("Sorry, no position available.");
+}
+
+var geo_options = {
+  enableHighAccuracy: true, 
+  maximumAge        : 60000, 
+  timeout           : 30000
+};
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(geo_success, geo_error, geo_options);
+  var wpid = navigator.geolocation.watchPosition(wgeo_success, geo_error, geo_options);
+} else {
+  /* geolocation IS NOT available */
+}
